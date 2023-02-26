a287f7ca4561fa9bcd5a8d63b91bf378223bd5df
exul7 <116938294+exul7@users.noreply.github.com>
2023-02-26 12:14:44 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-TOKEN = '...' #Сюда вы пишите свой токен. Узнать его можно в @BotFather  
+TOKEN = '6100918294:AAFBS2gYOrQgeezske5fdorlbcVu_WErUFM' #Сюда вы пишите свой токен. Узнать его можно в @BotFather  
